6414e1e934c003034fde676c1fe991248d7a9843
Bruno Martins <bgcngm@gmail.com>
2020-10-28 15:09:44 +0000

    msm8998-common: init: Adapt libinit to R
    Change-Id: Ib662f520500007c342bbcefdfdd71159f9f919a6

--- init/libinit_nokia.cpp
+++ init/libinit_nokia.cpp
@@ -40,7 +40,6 @@
 #include "property_service.h"
 
 using android::base::GetProperty;
-using android::init::property_set;
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -48,6 +47,17 @@ char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 char const *heaptargetutilization;
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
 
 void check_device()
 {
@@ -86,10 +96,10 @@ void vendor_load_properties()
 {
     check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
