051f9ac0d076d26dcb20b36d64eac805c20597b5
Tuan Anh <tuan73176@gmail.com>
2023-12-24 17:33:40 +0700

    msm8998-common: Cleanup SEPolicy section
    Change-Id: Id9a796ebd1c6f0ecefe8ac09db45de81437052c4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,10 +103,9 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/firmware_mnt:/firmware
 
 # Sepolicy
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Split selinux policy
 PRODUCT_FULL_TREBLE_OVERRIDE := true
