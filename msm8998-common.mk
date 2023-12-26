0f647a70d9f9ce8c176013775704e3cf00e2ba48
Aayush Gupta <aayushgupta219@gmail.com>
2023-12-26 19:00:56 +0700

    msm8998-common: Build & conditionally enable AOSP NFC service
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I511394c8ad30b71abf4dd963aed9951270eb8d17

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -170,9 +170,15 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2-service \
     NfcNci \
+    SecureElement \
     Tag \
     com.android.nfc_extras
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(COMMON_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
 PRODUCT_PACKAGES += \
