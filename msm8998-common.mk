22bb201dd1064c54ec019843efb62364b35e03da
Tao Bao <tbao@google.com>
2023-12-28 17:37:03 +0700

    msm8998-common: AB_OTA_UPDATER is BoardConfig variable.
    [Tuan Anh: Guard vendor specifically for NB1]
    
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -21,13 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay
 
 # A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    system \
-    vendor
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
