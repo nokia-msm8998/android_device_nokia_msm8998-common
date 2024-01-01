346f2819cf00475da5618c05bb2f1628e1225cda
Tuan Anh <tuan73176@gmail.com>
2024-01-02 06:19:21 +0700

    msm8998-common: Copy seccomp policy to vendor
    * From A1N_sprout
    
    Change-Id: I2356cd9b5f7b96fc5d0d0d40cba96143ea7233a2

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -285,6 +285,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
