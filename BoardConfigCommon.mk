3db3083ec7861acf61a180a727a3370311ef2488
George <geoster3d@gmail.com>
2024-02-05 18:21:57 +0700

    msm8998-common: Import manifest from stock
    * From NB1_00WW_FIH-user-9-PPR1.180610.011-00WW_5_16A-release-keys
    * Remove sepolicy version
    
    Change-Id: I8c3f9675a15beb5bcee47b3cbec8f903de84b9a8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,6 +69,7 @@ TARGET_USES_HWC2 := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # HIDL
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Properties
