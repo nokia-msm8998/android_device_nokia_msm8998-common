0c8b37d62622c3b261cbf5ae356708778103c734
Tuan Anh <tuan73176@gmail.com>
2024-02-06 19:21:49 +0700

    msm8998-common: Build fingerprint HAL
    Change-Id: I16c80103b164e11914f34343ad45adc7f6c65396

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -144,6 +144,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # FM
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
