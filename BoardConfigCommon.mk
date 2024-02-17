284171059385855a75a80854fd70cf368a4546ee
dianlujitao <dianlujitao@lineageos.org>
2024-02-17 21:43:42 +0700

    msm8998-common: gps: Build new GNSS HAL
     * Remove unnecessary dependencies on liblbs_core.so
     * Hardcode vendor loc HIDL version to 3.0 to enable vendor HIDL when
       building w/o BSP.
    
    Change-Id: I01f661f1d1e038737801f5a9db88e3388d470f74

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,10 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 BOARD_HAS_QCA_FM_SOC := cherokee
 BOARD_HAVE_QCOM_FM := true
 
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 3.0
+
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_power_aware=1
