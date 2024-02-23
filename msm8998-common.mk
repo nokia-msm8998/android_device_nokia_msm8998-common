933bacd4080d0b87385855779ce6de139173d83e
Tuan Anh <tuan73176@gmail.com>
2024-02-23 15:33:27 +0700

    msm8998-common: Move overlay section according to alphabet sorting
    Change-Id: Ifc9e75b1536bc6c7c9a0b44070e7f1559093ec23

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -16,10 +16,6 @@
 
 COMMON_PATH := device/nokia/msm8998-common
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(COMMON_PATH)/overlay
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -265,6 +261,10 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(COMMON_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
