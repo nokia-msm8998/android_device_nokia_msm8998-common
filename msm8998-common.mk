52653d935d31b897e3a4a818cc41b887a50f827d
Alex Damaratski <alexeydomoratsky1@gmail.com>
2024-02-23 12:53:36 +0700

    msm8998-common: Build Mock Powerstats HAL
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Id9abdc668108e3a864056778acca1d2889316fe0

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -328,6 +328,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service.nokia_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
