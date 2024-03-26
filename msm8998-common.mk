4616c4179b47980bce659eca3e040a0fc0ac59b5
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2024-03-26 20:37:12 +0700

    msm8998-common: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -430,7 +430,9 @@ PRODUCT_COPY_FILES += \
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(COMMON_PATH)
+    $(COMMON_PATH) \
+    hardware/google/pixel \
+    hardware/google/interfaces
 
 # Telephony
 PRODUCT_PACKAGES += \
