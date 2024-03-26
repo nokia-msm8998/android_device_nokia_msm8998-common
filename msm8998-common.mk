e75e2245cc9e246b796d249f2399438aa0071883
Lunarixus <Nathan@lunarixus.dev>
2024-03-26 14:25:59 +0700

    msm8998-common: Build frameworks stats HAL
    * Fingerprint depends on it.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>
    Change-Id: I2aa7f5750a1c7c546d93c85a0b2af4c45ea05223

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -150,6 +150,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.frameworks.stats@1.0.vendor \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.1-service
 
