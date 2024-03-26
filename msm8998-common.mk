a83bb642d5433a55bc66772c4959c35cf5de4ac2
Alessandro Astone <ales.astone@gmail.com>
2024-03-26 13:51:18 +0700

    msm8998-common: Build and enable fastbootd
    Change-Id: I61b84562615466e2e722c638e5dddb3095e7c1cb

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
