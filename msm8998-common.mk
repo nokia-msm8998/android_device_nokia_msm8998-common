cbc646869a3f87108eb1f15c56a6f18af8a30b9f
Jakub Pawlowski <jpawlowski@google.com>
2024-03-26 14:00:21 +0700

    msm8998-common: Uprev Bluetooth Audio HAL to 2.1
    * 2.1 adds new functionalities required for Bluetooth LE Audio development.
    
    Change-Id: I11fa8665d3241a3c9476962805f7ddb44d09e6cd

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -81,7 +81,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
