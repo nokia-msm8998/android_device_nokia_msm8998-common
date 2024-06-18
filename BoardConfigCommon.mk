05d6c74e7025f427c4bff3a825b9ac689b0f0a7b
Bruno Martins <bgcngm@gmail.com>
2024-06-18 11:12:10 +0700

    msm8998-common: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
