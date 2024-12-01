7b73a7fa40c5d90754364c3d3634242d97583c7c
Michael Bestas <mkbestas@lineageos.org>
2024-12-01 22:30:22 +0200

    msm8998-common: gps/location: Convert remaining Android.mk to bp
    Change-Id: I75e6af92795fe755a48557aa06106406b14c6750

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,6 @@ BOARD_HAVE_QCOM_FM := true
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-LOC_HIDL_VERSION := 4.0
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
