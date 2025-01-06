e6dcb941617840e6a841a6b22714579a103c9408
Michael Bestas <mkbestas@lineageos.org>
2025-01-06 11:55:00 +0700

    msm8998-common: Remove thermal HAL
    * It's only used for VR, which we don't support
    
    Change-Id: I094dc805b3eb18553bf801e69c6a2591b6d784e4

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -383,11 +383,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
