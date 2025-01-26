08be9ec80cd4dcd57c1cde4dea140da5603386d1
snnbyyds <snnbyyds@gmail.com>
2025-01-26 15:46:43 +0700

    msm8998-common: Convert rootdir to init
    Use Android.bp as well.
    
    [Tuan Anh: Convert recovery init into a build target]
    
    Change-Id: Ifa7c2d37edd3cdac93ad7f0aca8d6eefeafd4d80

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,7 @@ TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_BLANK_UNBLANK_ON_INIT := true
 ifeq ($(filter NB1,$(shell echo $(TARGET_PRODUCT) | sed 's/^lineage_//')),)
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/init/fstab.qcom
 endif
 
 # Root
