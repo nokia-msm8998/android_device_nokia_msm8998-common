b55b3bc401d01915c2defb95b2c298115cb4ebc4
Łukasz Patron <priv.luk@gmail.com>
2025-10-06 04:12:49 +0000

    msm8998-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,10 +78,9 @@ BOARD_USES_ADRENO := true
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
