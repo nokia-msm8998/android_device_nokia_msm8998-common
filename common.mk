282ef98273f3a8914be90295cfd2ec978ee0711f
Tasos Sahanidis <tasos@tasossah.com>
2026-04-10 18:22:05 +0000

    msm8998-common: overlay: Disable turbulence noise
    This helps with jank when pulling down the status bar.
    
    Change-Id: Ifb7043492e9a38a58d7b1351355b47ec590ee5c7

--- common.mk
+++ common.mk
@@ -216,7 +216,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_PACKAGES += \
